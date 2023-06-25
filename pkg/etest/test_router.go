6a0aaf1bf65fdb02567d68303ab6239dd996df68
wjhdec <wjhdec@163.com>
2023-06-25 22:44:10 +0800

    test: 添加中间件参数

--- pkg/etest/test_router.go
+++ pkg/etest/test_router.go
@@ -2,26 +2,30 @@ package etest
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/wjhdec/echo-ext/pkg/config"
 	"github.com/wjhdec/echo-ext/pkg/server"
 )
 
-func LoadRouter(cfg config.Config, rfunc func(group *echo.Group, cfg config.Config) (server.Router, error)) (*server.Server, error) {
+func LoadRouter(cfg config.Config, routerFunc func(group *echo.Group, cfg config.Config) (server.Router, error), middleware ...echo.MiddlewareFunc) (*server.Server, error) {
 	serv, err := server.NewServer("test", cfg)
 	if err != nil {
 		return nil, err
 	}
 	baseGroup := serv.Echo().Group("")
-	serv.AddMiddleware(middleware.Recover(), middleware.Logger())
 	if err != nil {
 		return nil, err
 	}
-	r, err := rfunc(baseGroup, cfg)
+	if len(middleware) > 0 {
+		serv.AddMiddleware(middleware...)
+	}
+	if r, err := routerFunc(baseGroup, cfg); err != nil {
+		return nil, err
+	} else {
+		serv.AddRouter(r)
+	}
 	if err != nil {
 		return nil, err
 	}
-	serv.AddRouter(r)
 	serv.RegisterRouters()
 	return serv, nil
 }
